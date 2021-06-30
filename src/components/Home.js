57c6b96a08f86ddf15e5427efd59fce6bd1c975f
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-30 18:28:19 -0400

    created home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,28 @@
 import React from 'react' 
+import Figure from 'react-bootstrap/Figure'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function Home(){
     return(
         <div>
-            I am the home page.
+            <Container>
+                        <Figure>
+                            <Figure.Image
+                                width={400}
+                                height={400}
+                                alt="Parents Portal - The place for families to get connected"
+                                src="https://i.imgur.com/uIL00JK.png"
+                            />
+                        </Figure>
+                <Row>
+                    <Col>
+                        <h1>Welcome!</h1>
+                        <p>Hello and welcome to Parents Portal. Our site allows parents to create events for other parents and children to join as well as share reviews, ask questions, and post comments in our forum section. Please use the navigation bar above to move about the site.</p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,17 @@
 import React from 'react' 
 import {Navbar, Nav} from 'react-bootstrap'
 
+
 export default function NavBar(){
     return(
         <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand >Parents Portal☺</Navbar.Brand>
+            <Navbar.Brand >Parents Portal</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="#">Home</Nav.Link>
                     <Nav.Link href="#">Meetups</Nav.Link>
-                    <Nav.Link href="#">Posts</Nav.Link>
+                    <Nav.Link href="#">Forum</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
