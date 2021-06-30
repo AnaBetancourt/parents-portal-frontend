b03a40e35ffdc52f75be104459f56f1f9b5a8b5a
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-30 19:28:28 -0400

    connected components to their routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import MeetupsContainer from './containers/MeetupsContainer'
+import PostsContainer from './containers/PostsContainer'
 import { Route } from 'react-router-dom'
 
 
@@ -9,7 +11,9 @@ function App() {
   return (
     <>                            
       <Route path="/" component={NavBar} />
-      <Route exact path="/" component={Home} />          
+      <Route exact path="/" component={Home} />
+      <Route exact path="/meetups" component={MeetupsContainer} />
+      <Route exact path="/forum" component={PostsContainer} />          
     </>
   );
 }
