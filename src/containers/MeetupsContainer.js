98d236f217a110a524b09462c9c21098e0468f7e
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-30 19:50:19 -0400

    set up basic formatting and structure of meetup container

--- src/containers/MeetupsContainer.js
+++ src/containers/MeetupsContainer.js
@@ -1,14 +1,25 @@
 import React from 'react' 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Meetup from '../components/meetups/Meetup'
 import MeetupForm from '../components/meetups/MeetupForm'
 
 class MeetupsContainer extends React.Component{
     render(){
         return(
-            <div>
-                <MeetupForm />
-                <Meetup />
-            </div>
+            <>
+                <Container>
+                    <Row>
+                        <Col>
+                            <MeetupForm />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Meetup />
+                    </Row>
+                </Container>
+            </>
         )
     }
 }
