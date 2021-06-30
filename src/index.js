fd42e43687a4a9edd6100b3c72d1d1d700ae3e98
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-30 16:23:12 -0400

    installed needed libraries and set up store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import {createStore} from "redux"
+import MeetupsReducer from './reducers/MeetupsReducer'
+
+const store = createStore(MeetupsReducer)
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
