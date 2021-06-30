c56d974c5baa2579178935162d1045b2a280f886
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-29 21:28:43 -0400

    connected react-bootstrap and created NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+  
+import React from 'react' 
+import {Navbar, Nav} from 'react-bootstrap'
+
+export default function NavBar(){
+    return(
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand >Parents Portal☺</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#">Home</Nav.Link>
+                    <Nav.Link href="#">Meetups</Nav.Link>
+                    <Nav.Link href="#">Posts</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
