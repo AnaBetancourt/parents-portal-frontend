8131a0faba33118b5fd85331823daeaeeb07f6f4
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-07-01 19:22:02 -0400

    set basic layout for meetup component

--- src/components/meetups/Meetup.js
+++ src/components/meetups/Meetup.js
@@ -1,9 +1,13 @@
 import React from 'react' 
 
-export default function Meetup(){
+export default function Meetup({id, title, date, time, location, description, interested_count}){
     return(
         <div>
-            I am a meet-up.
+            <h1>{title}</h1>
+            <h3>{date} | {time} | {location}</h3>
+            <p>{description}</p>
+            <h4>Interested: {interested_count}</h4>
+            <br />
         </div>
     )
 }
