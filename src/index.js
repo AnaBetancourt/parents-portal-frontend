63fe4483d722fb2cd4cb41f70e80e397ada2b732
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-07-01 19:41:09 -0400

    began setting up app to use thunk middleware

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducers/CombinedReducer';
 
 const store = createStore(rootReducer)
