89329f027f1550c2f338ff7dd9ff8ecac1938c6a
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-07-01 20:24:17 -0400

    set up post form

--- src/components/posts/PostForm.js
+++ src/components/posts/PostForm.js
@@ -1,9 +1,61 @@
-import React from 'react' 
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
-export default function PostForm(){
-    return(
-        <div>
-            I am a Form for posts.
-        </div>
-    )
+class PostForm extends React.Component{
+
+    state = {
+        body: ""
+    }
+
+    setShow(type){
+        this.setState({
+            ...this.state,
+            show: type
+        })
+    }
+
+    handleClose = () => this.setShow(false)
+    handleShow = () => this.setShow(true)
+
+    handleChange = (e) => {
+        const type = e.target.name
+        const input = e.target.value
+
+        this.setState({
+            [type]: input
+        })
+    }
+
+    handleSubmit = (e) => {
+        console.log(this.state)
+    }
+
+    render(){
+
+        return(
+            <>
+                <Button variant="outline-primary" onClick={this.handleShow}>Add Post</Button>
+                <Modal show={this.state.show} onHide={this.handleClose} backdrop="static" keyboard={false}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Create New Post</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicBody">
+                                <Form.Label>What would you like to say?</Form.Label>
+                                <Form.Control onChange={this.handleChange} as="textarea" rows={3} placeholder="Enter your text here." name="body" />
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" type="submit" onClick={this.handleSubmit}>Submit</Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        )
+    }
 }
+
+export default PostForm
