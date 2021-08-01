323f8e1510bdb90cff2ddd01ed0a34bd6b8474ad
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-08-01 16:13:16 -0400

    fixed minor issue in navbar that left 'home' page always selected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,18 @@
 import React from 'react' 
 import {Navbar, Nav} from 'react-bootstrap'
-import {NavLink} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 
 export default function NavBar(){
     return(
         <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand >Parents Portal</Navbar.Brand>
+            <Navbar.Brand>Parents Portal</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
-                    <Nav.Link as={NavLink} to='/meetups'>Meetups</Nav.Link>
-                    <Nav.Link as={NavLink} to='/forum'>Forum</Nav.Link>
+                    <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                    <Nav.Link as={Link} to='/meetups'>Meetups</Nav.Link>
+                    <Nav.Link as={Link} to='/forum'>Forum</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
